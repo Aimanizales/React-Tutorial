3f87c67e5c5043a15605545bddb163cd827af380
Aiman <aimanizales@gmail.com>
2018-06-04 00:58:38 -0500

    Added winner

--- src/index.js
+++ src/index.js
@@ -30,7 +30,10 @@ class Board extends React.Component {
 	}
 
 	handleClick(i) {
-		const squares = this.state.squares.slice(); 
+		const squares = this.state.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
 		/**
 		 * slice() > operator to copy the squares array prior to making changes 
 		 * and to prevent mutating the existing array
@@ -53,7 +56,8 @@ class Board extends React.Component {
 	}
 	
 	render() {
-		const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		const winner = calculateWinner(this.state.squares);
+		let status = winner ? ('Winner: ' + winner) : ('Next Player: ' + (this.state.xIsNext ? 'X' : 'O'));
 		
 		return (
 			<div>
@@ -104,3 +108,23 @@ ReactDOM.render(
 	<Game />,
 	document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+	const lines = [ 
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+};
