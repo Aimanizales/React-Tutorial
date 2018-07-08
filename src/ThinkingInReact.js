3baac376ad543359809fbd542bc40bac90962042
Aiman <aimanizales@gmail.com>
2018-07-08 14:02:29 -0500

    Added Thinking in React + Inverted index.html render

---
+++ src/ThinkingInReact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class ThinkingInReact extends React.Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<h3>4. Thinking In React</h3>
+					<p>
+						<a href="https://reactjs.org/docs/thinking-in-react.html" target="_blank" rel="noopener noreferrer">https://reactjs.org/docs/thinking-in-react.html</a>
+					</p>
+					<p>
+						In this document, we’ll walk you through the thought process of building a searchable product data table using React.
+					</p>
+					<hr/>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default ThinkingInReact;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import { Game } from './Game';
 import { FormTutorial } from './FormTutorial';
 import LiftingStateUp from './LiftingStateUp';
+import ThinkingInReact from './ThinkingInReact';
 import './index.css';
 
 const container1 = document.getElementById('root');
 const container2 = document.getElementById('root2');
 const container3 = document.getElementById('root3');
+const container4 = document.getElementById('root4');
 
 ReactDOM.render(<FormTutorial />, container1);
-ReactDOM.render(<Game />, container2);ReactDOM.render(<LiftingStateUp />, container3);
+ReactDOM.render(<Game />, container2);
+ReactDOM.render(<LiftingStateUp />, container3);
+ReactDOM.render(<ThinkingInReact />, container4);
 
 /* 
 ReactDOM.render(<input type="text" value="hi" />, container3);
